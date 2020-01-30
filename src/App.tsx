9ddf1ea312bcc071ce6d9912bc8ea69b7e40a51e
ik <salah.lamkadem@gmail.com>
2020-01-30 14:53:41 +0100

    fix: 🐛 main ui handle route switch
    removed route switch logique into mainUi to render only once mainUi and
    reredener only route view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
-import Dashboard from 'components/pages/Dashboard';
-import Companies from 'components/pages/Companies';
-import Pois from 'components/pages/Pois';
 import MainUi from 'components/template/MainUi/MainUi';
 
 import './App.css';
@@ -12,17 +9,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <MainUi render={<Dashboard />} />
-          </Route>
-          <Route path="/companies">
-            <MainUi render={<Companies />} />
-          </Route>
-          <Route path="/pois">
-            <MainUi render={<Pois />} />
-          </Route>
-        </Switch>
+        <MainUi />
       </Router>
     </div>
   );
