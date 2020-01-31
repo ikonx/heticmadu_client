8583cbb8d5a929215b7458fd5f56286178869ebb
doriantaing <doriantaing77@hotmail.fr>
2020-01-31 12:07:28 +0100

    remove unused files

--- src/components/molecules/Table/head/_style.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { TableCell } from "@material-ui/core";
-
-export const TableHeadCell = styled(TableCell)`
-  border: none !important;
-  padding-top: 0 !important;
-  padding-bottom: .5rem !important;
-`;
