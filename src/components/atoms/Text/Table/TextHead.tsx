4bbc37be7014a1c7446d3c69add9e578687aa4cc
doriantaing <doriantaing77@hotmail.fr>
2020-02-17 17:13:37 +0100

    add text head

---
+++ src/components/atoms/Text/Table/TextHead.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Typography } from "@material-ui/core";
+import styled from "styled-components";
+import { Colors } from "../../../../utils/styles";
+
+interface Props {
+  name: string
+}
+
+const TextContainer = styled.div`
+  display: flex;
+  align-items: center;
+  color: #828282;
+  cursor: pointer;
+`;
+
+const Text = styled(Typography)`
+  && {
+    color: ${Colors.black};
+    font-size: 12px;
+  }
+`;
+
+const TextHead: React.FC<Props> = ({ name }) => {
+  return (
+    <TextContainer>
+      <Text>
+          {name}
+      </Text>
+    </TextContainer>
+  );
+};
+
+export default TextHead;
