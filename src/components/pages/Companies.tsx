573e28d0f502c870cb9b17de575f793e33c36fff
doriantaing <doriantaing77@hotmail.fr>
2020-02-17 10:54:32 +0100

    add map

--- src/components/pages/Companies.tsx
+++ src/components/pages/Companies.tsx
@@ -2,22 +2,37 @@ import React from 'react'
 import styled from "styled-components";
 import { index as TableItem } from '../molecules/Table/Row';
 import { index as TableHead } from '../molecules/Table/Head';
-import { Table, TableBody, TableRow } from "@material-ui/core";
+import { Grid, Table, TableBody, TableRow } from "@material-ui/core";
 import PageHeader from "../molecules/PageHeader/PageHeader";
+import ReactMapboxGl from 'react-mapbox-gl';
+
 
 const CompanyContainer = styled.section`
-  width: calc(100% - 80px); 
-  margin: 24px 40px 0;
+  width: 100%;
+  min-height: calc(100vh - 64px); 
+  margin: 0;
 `;
 
 const TableSpace = styled(TableRow)`
-  border: 0 !important;
-  padding: 12px !important;
-  height: 24px;
+  && {
+    border: 0;
+    padding: 12px;
+    height: 24px;
+  }
 `;
 
 const CompanyTable = styled(Table)`
   margin-top: 24px;
+`;
+
+const Map = ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOXGL_KEY || '',
+});
+
+const LeftColumn = styled(Grid)`
+  && {
+    padding: 2rem 2rem 0;
+  }
 `;
 
 interface Props {}
@@ -25,18 +40,34 @@ interface Props {}
 const Companies: React.FC<Props> = () => {
     return (
         <CompanyContainer>
-            <PageHeader/>
-            <CompanyTable>
-                <TableHead />
-                <TableBody>
-                    {/* TODO: @DEV Loop over */}
-                    <TableItem params={{ withLogo: true, withGreenScore: false }}/>
-                    <TableSpace />
-                    <TableItem params={{ withLogo: true, withGreenScore: false }}/>
-                    <TableSpace />
-                    <TableItem params={{ withLogo: true, withGreenScore: false }}/>
-                </TableBody>
-            </CompanyTable>
+            <Grid container>
+                <LeftColumn item xs={7}>
+                    <PageHeader/>
+                    <CompanyTable>
+                        <TableHead />
+                        <TableBody>
+                            {/* TODO: @DEV Loop over */}
+                            <TableItem params={{ withLogo: true, withGreenScore: false }}/>
+                            <TableSpace />
+                            <TableItem params={{ withLogo: true, withGreenScore: false }}/>
+                            <TableSpace />
+                            <TableItem params={{ withLogo: true, withGreenScore: false }}/>
+                        </TableBody>
+                    </CompanyTable>
+                </LeftColumn>
+                <Grid item xs={5}>
+                    <Map
+                      style="mapbox://styles/mapbox/streets-v9"
+                      containerStyle={{
+                        minHeight: 'calc(100vh - 70px)',
+                        width: '100%',
+                        maxWidth: 'calc(100vw - 280px)',
+                      }}
+                      movingMethod="flyTo"
+                      center={[2.349014, 48.864716]}
+                    />
+                </Grid>
+            </Grid>
         </CompanyContainer>
     )
 }
