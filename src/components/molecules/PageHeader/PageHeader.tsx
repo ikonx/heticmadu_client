77a39024feaecdffefe560959c9aa25b3d611426
ik <salah.lamkadem@gmail.com>
2020-02-18 16:13:46 +0100

    router add and edit POI

--- src/components/molecules/PageHeader/PageHeader.tsx
+++ src/components/molecules/PageHeader/PageHeader.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { PageHeaderContainer } from "./_style";
 import MainTitle from "../../atoms/Text/MainTitle";
 import BtnBlue from "../../atoms/Buttons/BtnBlue";
-
+// @TODOS remove onAddItemProp
 interface Props {
   title: string,
   text: string,
   subtitle: string,
   link: string
-  onAddItem: () => void;
+  onAddItem?: () => void;
 }
 
 const PageHeader: React.FC<Props> = ({title, text, subtitle, link, onAddItem}) => {
   return (
     <PageHeaderContainer>
       <MainTitle title={title} subtitle={subtitle}/>
-      <BtnBlue text={text} link={link} onClick={onAddItem} />
+      <BtnBlue text={text} link={link} />
     </PageHeaderContainer>
   );
 };
