2085adb3309609673ca1189bc016933690b35751
Theo-LARUE <theo.larue@hotmail.com>
2020-02-18 17:15:34 +0100

    fix login

--- src/components/molecules/Table/Row/_style.tsx
+++ src/components/molecules/Table/Row/_style.tsx
@@ -23,7 +23,7 @@ export const Cell = styled(TableCell)`
     }
 `;
 
-export const CellActive = styled(Typography)<{active: number}>`
+export const CellActive = styled(Typography)<{active: boolean}>`
     && {
       background: ${props => props.active ? Colors.green : Colors.red};
       color: ${props => props.active ? Colors.darkGreen : Colors.darkRed};
