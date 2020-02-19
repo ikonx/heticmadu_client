4e00a54e1f304ead0200653bdd5e1ded5994914d
ik <salah.lamkadem@gmail.com>
2020-02-19 18:06:16 +0100

    onChange prop

--- src/components/atoms/Inputs/Radio/InputRadio.tsx
+++ src/components/atoms/Inputs/Radio/InputRadio.tsx
@@ -1,38 +1,49 @@
 import React from 'react';
-import {RadioContainer, StyledLabel, StyledRadio, StyledRadioGroup, StyledTitle} from "./_style";
+import {
+  RadioContainer,
+  StyledLabel,
+  StyledRadio,
+  StyledRadioGroup,
+  StyledTitle,
+} from './_style';
 
 interface Props {
   title: string;
   values: {
-    name: string,
-    category: string
-  }[]
+    name: string;
+    category: string;
+  }[];
+  onChange?: (value: any) => void;
 }
 
-const InputRadio: React.FC<Props> = ({ title, values }) => {
-  const [selectedValue, setSelectedValue] = React.useState<string | undefined>(values[0].name);
+const InputRadio: React.FC<Props> = ({ title, values, onChange }) => {
+  const [selectedValue, setSelectedValue] = React.useState<string | undefined>(
+    values[0].name,
+  );
 
   const handleChange = (event: { target: HTMLInputElement }) => {
     const { value } = event.target;
     setSelectedValue(value);
+    onChange && onChange(value);
   };
 
   return (
     <div>
-      <StyledTitle>{ title }</StyledTitle>
+      <StyledTitle>{title}</StyledTitle>
       <RadioContainer>
-        { values.length > 0 && values.map((item, i) => (
-          <StyledRadioGroup key={i}>
-            <StyledRadio
-              type="radio"
-              name={item.category}
-              value={item.name}
-              onChange={handleChange}
-              checked={item.name === selectedValue}
-            />
-            <StyledLabel>{ item.name }</StyledLabel>
-          </StyledRadioGroup>
-        ))}
+        {values.length > 0 &&
+          values.map((item, i) => (
+            <StyledRadioGroup key={i}>
+              <StyledRadio
+                type="radio"
+                name={item.category}
+                value={item.name}
+                onChange={handleChange}
+                checked={item.name === selectedValue}
+              />
+              <StyledLabel>{item.name}</StyledLabel>
+            </StyledRadioGroup>
+          ))}
       </RadioContainer>
     </div>
   );
--- src/components/atoms/Select/CustomSelect.tsx
+++ src/components/atoms/Select/CustomSelect.tsx
@@ -1,24 +1,33 @@
 import React, { ChangeEvent, useState } from 'react';
-import { StyledContainer, StyledInput, StyledInputValue, StyledTitle } from "./_style";
+import {
+  StyledContainer,
+  StyledInput,
+  StyledInputValue,
+  StyledTitle,
+} from './_style';
 
 interface Props {
-  title: string,
-  values: string[]
+  title: string;
+  values: string[];
+  onChange?: (value: any) => void;
 }
 
-const CustomSelect: React.FC<Props> = ({ title, values }) => {
+const CustomSelect: React.FC<Props> = ({ title, values, onChange }) => {
   const [selectValue, setSelectValue] = useState(values[0]);
-  const selectChange = (event: React.ChangeEvent<{ value: any }> ) => {
+  const selectChange = (event: React.ChangeEvent<{ value: any }>) => {
     const { value } = event.target;
     setSelectValue(value);
+    onChange && onChange(value);
   };
 
   return (
     <StyledContainer>
-      <StyledTitle>{ title }</StyledTitle>
+      <StyledTitle>{title}</StyledTitle>
       <StyledInput value={selectValue} displayEmpty onChange={selectChange}>
-        { values.map((item, i) => (
-          <StyledInputValue value={item} key={i}>{ item }</StyledInputValue>
+        {values.map((item, i) => (
+          <StyledInputValue value={item} key={i}>
+            {item}
+          </StyledInputValue>
         ))}
       </StyledInput>
     </StyledContainer>
