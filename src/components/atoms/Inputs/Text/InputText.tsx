0543babd52228546f2f38c9513116737f5d7e9c4
ik <salah.lamkadem@gmail.com>
2020-02-20 22:06:34 +0100

    updated props key field and field value

--- src/components/atoms/Inputs/Text/InputText.tsx
+++ src/components/atoms/Inputs/Text/InputText.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import { StyledFormControl, StyledInput, StyledLabel } from "./_style";
+import { StyledFormControl, StyledInput, StyledLabel } from './_style';
 
 interface Props {
   title: string;
   placeholder: string;
+  fieldKey?: string;
+  onChange?: (_fieldKey: string, _fieldValue: string) => void;
+  fieldValue?: string;
+  type?: string;
 }
 
-const InputText: React.FC<Props> = ({ title, placeholder }) => {
+const InputText: React.FC<Props> = ({
+  title,
+  placeholder,
+  onChange,
+  fieldValue,
+  fieldKey,
+  type
+}) => {
   return (
     <StyledFormControl>
-      <StyledLabel htmlFor={title}>{ title }</StyledLabel>
+      <StyledLabel htmlFor={title}>{title}</StyledLabel>
       <StyledInput
         id={title}
-        type="text"
+        type={type || "text"}
         placeholder={placeholder}
+        defaultValue={fieldValue || ''}
+        onBlur={e => {
+          onChange && onChange(fieldKey || '', e.target.value || '');
+        }}
       />
     </StyledFormControl>
   );
