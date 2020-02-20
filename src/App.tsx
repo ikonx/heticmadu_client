f9a4f10a6c22a7f5a3475518aef1251e2e0b33ef
ik <salah.lamkadem@gmail.com>
2020-02-20 17:32:01 +0100

    added pois provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 
 import MainUi from 'components/template/MainUi/MainUi';
 
 import './App.css';
 import Login from 'components/pages/Login';
+import PoisProvider from 'contexts/pois/pois.provider';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
-          <Switch>
-            <Route exact path="/">
-              <Login />
-            </Route>
-            <Route path="*">
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="*">
+            <PoisProvider>
               <MainUi />
-            </Route>
+            </PoisProvider>
+          </Route>
         </Switch>
       </Router>
     </div>
