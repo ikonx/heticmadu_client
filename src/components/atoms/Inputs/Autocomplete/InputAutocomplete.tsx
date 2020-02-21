e7cfdf0c114910d29b0827c1162baf5a9cbfc5fb
ik <salah.lamkadem@gmail.com>
2020-02-21 18:37:34 +0100

    changed props + onChange to get lgtd latd etc

--- src/components/atoms/Inputs/Autocomplete/InputAutocomplete.tsx
+++ src/components/atoms/Inputs/Autocomplete/InputAutocomplete.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useRef } from 'react';
 import places from 'places.js';
-import { StyledFormControl, StyledInput, StyledLabel } from "./_style";
-
+import { StyledFormControl, StyledInput, StyledLabel } from './_style';
 
 interface Props {
   title: string;
   placeholder: string;
   fieldKey?: string;
   fieldValue?: any;
-  onChange?: (_fieldKey: string, _fieldValue: any) => void;
+  onChange?: (_fieldKey: any, _fieldValue: any) => void;
 }
 
-const InputAutocomplete: React.FC<Props> = ({ title, placeholder }) => {
+const InputAutocomplete: React.FC<Props> = ({
+  title,
+  placeholder,
+  onChange,
+  fieldKey,
+  fieldValue,
+}) => {
   const search = useRef(null);
 
   useEffect(() => {
@@ -26,12 +31,44 @@ const InputAutocomplete: React.FC<Props> = ({ title, placeholder }) => {
     };
 
     places(options).configure(reconfigurableOptions);
+    places(options).on('change', (e: any) => {
+      const parsedData = {
+        longitude: e.suggestion.latlng.lng,
+        latitude: e.suggestion.latlng.lat,
+        address: e.suggestion.value,
+      };
+
+      onChange && onChange(fieldKey, parsedData);
+
+      // administrative: "Île-de-France"
+      // city: "Paris"
+      // country: "France"
+      // countryCode: "fr"
+      // county: "Paris"
+      // highlight: {name: "<em>Paris</em> 10e Arrondissement", city: "<em>Paris</em>", administrative: "Île-de-France", country: "France", county: "<em>Paris</em>", …}
+      // hit: {country: "France", is_country: false, city: Array(1), is_highway: false, importance: 15, …}
+      // hitIndex: 1
+      // latlng: {lat: 48.8718, lng: 2.35779}
+      // name: "Paris 10e Arrondissement"
+      // postcode: "75010"
+      // postcodes: ["75010"]
+      // query: "paris"
+      // rawAnswer: {hits: Array(5), nbHits: 5, processingTimeMS: 6, query: "paris", params: "hitsPerPage=5&language=fr&query=paris", …}
+      // type: "city"
+      // value: "Paris 10e Arrondissement, Paris, Île-de-France, France"
+    });
   }, []);
 
   return (
     <StyledFormControl>
-      <StyledLabel>{ title }</StyledLabel>
-      <StyledInput type="text" ref={search} className="form-control" placeholder={placeholder}/>
+      <StyledLabel>{title}</StyledLabel>
+      <StyledInput
+        type="text"
+        ref={search}
+        className="form-control"
+        placeholder={placeholder}
+        defaultValue={fieldValue}
+      />
     </StyledFormControl>
   );
 };
