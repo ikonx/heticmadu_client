26d54046585ed1cc822631ef9657d41a50603f09
ik <salah.lamkadem@gmail.com>
2020-03-27 20:12:39 +0100

    feat: 🎸 login

--- src/contexts/user/user.provider.tsx
+++ src/contexts/user/user.provider.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { useHistory } from 'react-router-dom';
+import * as jwtDecode from 'jwt-decode';
 import UserContext from './user.context';
 import { UserModel } from 'utils/models/user.model';
 import { loginUser as loginUserAPI } from 'utils/http';
-import { useHistory } from 'react-router-dom';
 
 interface Props {}
 
@@ -25,13 +27,35 @@ const UserProvider: React.FC<Props> = ({ children }) => {
       const token = response.data;
       setUser({ ...user, token, isLogin: true, gotError: false });
       history.push('/');
+      localStorage.setItem('token', token);
     } else {
       setUser({ ...user, token: '', isLogin: false, gotError: true });
     }
   };
 
+  const checkToken = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const { exp, iat } = jwtDecode.default(token);
+      const expDate = moment.unix(exp);
+      const now = moment();
+
+      if (now.isAfter(expDate)) {
+        setUser({ ...user, token: '', isLogin: false });
+      } else {
+        history.push('/');
+        setUser({ ...user, token, isLogin: true });
+      }
+    }
+  };
+
   useEffect(() => {
-    !user.isLogin && history.push('/login');
+    checkToken();
+  }, []);
+
+  useEffect(() => {
+    user.isLogin === false && history.push('/login');
+    user.isLogin === false && checkToken();
   }, [user.isLogin]);
 
   return (
