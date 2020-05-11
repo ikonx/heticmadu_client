040ea51d3e4f40c1c0bd2c007ef90ffcb3cfb1f5
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-05-11 14:39:14 +0200

    Fixed local docker, added prod docker.

--- devops/scripts/build_enc.sh
+++
@@ -1,4 +0,0 @@
-cp ~/.ssh/heticmadu ./devops/secrets/private_key
-tar cvf secrets.tar ./devops/secrets/private_key ./devops/secrets/.env
-travis encrypt-file secrets.tar ./devops/secrets/secrets.tar.enc --com
-rm ./secrets.tar
